033e2a9e2f347e9334149d55c21a1c51479077c8
David Richfield <davidrichfield@gmail.com>
2016-11-17 21:56:14 +0100

    Start creating script to scale piece thickness and indent

---
+++ ScaleStuff.py
@@ -0,0 +1,13 @@
+import bpy
+
+#Grab all objects called Shell*
+shells = [obj for obj in bpy.data.objects if obj.name.startswith("Shell")]
+
+#From http://blender.stackexchange.com/questions/2638/run-an-edit-mode-operator-on-every-object-in-the-scene
+#Iterate through all objects, going into and out of edit mode
+for obj in shells:
+     bpy.context.scene.objects.active = obj
+     bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+     bpy.ops.mesh.reveal()
+     bpy.ops.mesh.select_all(action='SELECT')
+     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
